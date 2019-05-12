fa1138c065e0fe3d945b47491c3251fae14dc7b2
Lobster <alexrudyk16@gmail.com>
2019-05-12 18:48:09 +0300

    created entity for user; added datasource url to app properties

---
+++ src/main/java/com/lobster/testtask/User.java
@@ -0,0 +1,69 @@
+package com.lobster.testtask;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String login;
+
+    @Column
+    private String fullName;
+
+    @Column
+    private LocalDate dateOfBirth;
+
+    @Column
+    private String gender;
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
